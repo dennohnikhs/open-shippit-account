f4de06dc90824fdf7c02c943dfd660d03904e74b
Langat <davidsonlangat2021@gmail.com>
2024-05-29 23:16:34 +0300

    initial commit

---
+++ src/app/open-account/open-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './open-account.component.html',
+  styleUrl: './open-account.component.scss'
+})
+export class OpenAccountComponent {
+
+}
